525b7009d95278f75d85c4fade4d5d089ef0591b
Anthony Khaimov <165203674+AKhai47@users.noreply.github.com>
2025-11-20 19:19:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ limiter = Limiter(
     app=app,
     key_func=get_remote_address,
     default_limits=["200 per hour"],
-    storage_uri=os.getenv('REDIS_URL', 'memory://')
+    storage_uri=os.getenv('REDIS_URL', 'memory://'),
     exempt_routes=['/health']
 )
 
